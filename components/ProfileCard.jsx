9da9cdf4e4aeba136edbcf6df5341b97d02a59fe
Ivan Czar <ivanczar2013@gmail.com>
2023-06-15 09:19:30 +1200

    fix

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,12 +1,14 @@
 import { IoLogoLinkedin } from 'react-icons/io';
 
-const ProfileCard = () => {
+const ProfileCard = ({ name, position, linkedinURL, image }) => {
   return (
     <div class="bg-red-200 h-52 w-40 relative bg-[url('/ivan.jpg')] bg-cover">
       <div class='absolute bg-white w-full h-full opacity-0 hover:opacity-80 ease-in-out transition-all duration-300 flex flex-col flex-center'>
-        <h3 class='font-bold'>Ivan Czar</h3>
-        <p>Treasurer</p>
-        <IoLogoLinkedin class='text-3xl mt-2' />
+        <h3 class='font-bold'>{name}</h3>
+        <p>{position}</p>
+        <a href={linkedinURL} target='_blank'>
+          <IoLogoLinkedin href={linkedinURL} class='text-3xl mt-2' />
+        </a>
       </div>
     </div>
   );
