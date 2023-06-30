9a9618709da8c88f91c7fd0afac7108f074f46f4
Ivan Czar <ivanczar2013@gmail.com>
2023-06-30 23:41:02 +1200

    optimized fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Nav from '@components/Nav'
 import Footer from '@components/Footer'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
 import CookieBanner from '@components/CookieBanner'
+import { InterFont } from '@utils/fonts'
 
 export const metadata = {
     title: 'CSEA | Home',
@@ -13,7 +14,7 @@ const RootLayout = ({ children }) => {
     return (
         <html lang="en">
             <GoogleAnalytics GA_MEASUREMENT_ID="G-67JGV4E15D" />
-            <body>
+            <body className={InterFont.className}>
                 <main>
                     <Nav />
                     {children}
