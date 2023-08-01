47823de5f150f822fdff76d91a44374216913f47
ivanczar <ivanczar2013@gmail.com>
2023-08-01 20:06:23 +1200

    fixed about header image

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -10,6 +10,7 @@ const ProfileCard = ({ name, position, linkedinURL, image }) => {
                 src={`https://res.cloudinary.com/${process.env.CLOUDINARY_ID}/image/upload/q_auto/fl_lossy/f_auto/${image}`}
                 sizes="50vw"
                 alt={name}
+                loading="lazy"
             ></Image>
 
             <div class="absolute bg-white w-full h-full opacity-0 hover:opacity-80 ease-in-out transition-all duration-300 flex flex-col flex-center">
