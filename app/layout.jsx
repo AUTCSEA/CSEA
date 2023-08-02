cf760579194385e5878ba5fcf7094870b345ae85
ivanczar <ivanczar2013@gmail.com>
2023-08-02 22:26:39 +1200

    fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,15 +4,18 @@ import Footer from '@components/Footer'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
 import CookieBanner from '@components/CookieBanner'
 import { InterFont } from '@utils/fonts'
+import { Suspense } from 'react'
 
 export const metadata = {
-    metadataBase: new URL('http://csea.co.nz'),
+    metadataBase: new URL('https://csea.co.nz'),
 }
 
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
-            <GoogleAnalytics GA_MEASUREMENT_ID="G-67JGV4E15D" />
+            <Suspense fallback={<div>Loading...</div>}>
+                <GoogleAnalytics GA_MEASUREMENT_ID="G-67JGV4E15D" />
+            </Suspense>
             <body className={InterFont.className}>
                 <main>
                     <Nav />
